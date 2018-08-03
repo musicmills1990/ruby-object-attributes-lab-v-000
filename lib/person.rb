eaab8e80629d91f6b767998de2778ebe27a71870
Marc Mills <mdmills1990@gmail.com>
2018-08-03 18:47:27 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,25 +1,25 @@
 class Person
-def initilize_person(name)
-  @name = name
-end
+  def initilize_person(name)
+    @name = name
+  end
 
-def name
-  @name
-end
+  def name
+    @name
+  end
 
-def name=(peson_name)
-  @name = person_name
-end
+  def name=(peson_name)
+    @name = person_name
+  end
 
-def initilize_job(job)
-  @job = job
-end
+  def initilize_job(job)
+    @job = job
+  end
 
-def job
-  @job
-end
+  def job
+    @job
+  end
 
-def job=(job_name)
-  @job = job_name
-end
+  def job=(job_name)
+    @job = job_name
+  end
 end
